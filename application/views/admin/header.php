c1ec44d2a7007e0b23671a68d0928173a2906159
bloodmoonshard <bloodmoonshard@gmail.com>
2014-11-13 16:10:16 +0700

    13_11

--- application/views/admin/header.php
+++ application/views/admin/header.php
@@ -75,7 +75,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
     </button>
-    <a class="navbar-brand" href="/" target="_blank">Авантелт</a>
+    <a class="navbar-brand" href="/" target="_blank">Авантэлт</a>
 </div>
 <!-- /.navbar-header -->
 
